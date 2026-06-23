4e04f4de07462bbbd594831524ede65d28647ce4
kainansouza-bit <kainan.souza@escola.pr.gov.br>
2026-06-23 11:02:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function mostrarSecao(id) {
+  const secoes = document.querySelectorAll(".secao");
+  secoes.forEach(secao => secao.classList.remove("ativa"));
 
+  document.getElementById(id).classList.add("ativa");
+}
+
+function mostrarFato() {
+  const fatos = [
+    "O Brasil é um dos maiores exportadores de soja do mundo.",
+    "Drones ajudam a monitorar plantações em tempo real.",
+    "A tecnologia reduz o desperdício de água no campo."
+  ];
+
+  const aleatorio = fatos[Math.floor(Math.random() * fatos.length)];
+  document.getElementById("fato").innerText = aleatorio;
+}
+
+function responder(resposta) {
+  const resultado = document.getElementById("resultado");
+
+  if (resposta) {
+    resultado.innerText = "❌ Incorreto! O agronegócio também envolve indústria e transporte.";
+  } else {
+    resultado.innerText = "✅ Correto! Ele envolve agricultura, indústria e distribuição.";
+  }
+}
